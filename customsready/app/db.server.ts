d9d09482467979bd2eecdb1c270225762b676f98
tahatms12 <tahatms@gmail.com>
2026-03-19 01:45:23 +0500

    Refactor PrismaClient initialization in db.server.ts

--- customsready/app/db.server.ts
+++ customsready/app/db.server.ts
@@ -1,4 +1,3 @@
-// app/db.server.ts
 import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
 
@@ -6,18 +5,7 @@ declare global {
   // eslint-disable-next-line no-var
   var __prisma: PrismaClient | undefined;
 }
-export const db: PrismaClient =
-  global.__prisma ??
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
 
-// Prevent multiple PrismaClient instances in development (HMR)
 export const db: PrismaClient =
   global.__prisma ??
   new PrismaClient({
